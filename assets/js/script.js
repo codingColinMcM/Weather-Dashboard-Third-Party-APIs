1cd5e3402a2dea262e73ec8ff7be5c3c2680d1a4
Colin McMurtray <mcmurtraycolin@gmail.com>
2023-05-09 14:56:19 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,7 +36,7 @@ function savedCity(location) {
 
   displayList(location);
 }
-// FFS, PLEASE WORK!!! GOD DAMNIT!!!!!
+// OMFG, PLEASE WORK!!! GOD DAMNIT!!!!!
 // make searched cities into a list of cities, and append to HTML
 function displayList(location) {
   for (var i = 0; i < historyArray.length; i++) {
