a3c68abc4e88b30b60854138ea4146bd4ae7ad90
Colin McMurtray <mcmurtraycolin@gmail.com>
2023-05-09 12:23:01 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -219,7 +219,7 @@ function renderForecastCard(forecast, timezone) {
 
     forecastContainer.append(col);
 }
-//PLEASE WORK!!!
+//PLEASE WORK AND UPDATE!!!
 function handleSearch(e) {
 
     if(!searchInput.value) {
